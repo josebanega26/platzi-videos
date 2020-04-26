f4e72f8dd68d069e9c30a940524c865e89c560d6
Jose Banega <banegajose26@gmail.com>
2020-04-26 01:45:57 -0300

    Add Header,Footer & Layout Components, with style

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "../assets/styles/app.scss";
-
+import Layout from "../components/Layout";
 function App() {
-  return <h1>Finish config </h1>;
+  return (
+    <Layout>
+      <h1>Finish config </h1>
+    </Layout>
+  );
 }
 
 export default App;
